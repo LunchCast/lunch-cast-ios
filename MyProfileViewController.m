772a2aa61b657fc2149d2a4856c4f6e34935cbe3
Aleksandra Stevovic <aleksandra.stevovic@Aleksandras-MacBook-Pro.local>
2016-06-29 22:18:02 +0300

    Fixed bug in OrderVC + some fields in ProfileVC

--- MyProfileViewController.m
+++ MyProfileViewController.m
@@ -15,9 +15,7 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *labelEmail;
 @property (weak, nonatomic) IBOutlet UITextField *textFieldName;
-@property (weak, nonatomic) IBOutlet UILabel *labelOrdersDone;
-@property (weak, nonatomic) IBOutlet UILabel *labelFavoriteFood;
-@property (weak, nonatomic) IBOutlet UILabel *labelFavoriteRestaurant;
+@property (weak, nonatomic) IBOutlet UITextField *floorTextField;
 
 @end
 
@@ -30,6 +28,7 @@
     BackendlessUser *user = backendless.userService.currentUser;
     self.textFieldName.text = user.name.length ==0 ? @"User" : user.name;
     self.labelEmail.text = user.email;
+//    self.floorTextField.text = user.floor.length ==0 ? @"Floor" : user.floor;
 }
 
 - (IBAction)saveButtonAction:(UIBarButtonItem *)sender
@@ -41,6 +40,7 @@
           response:^(BackendlessUser *updatedUser) {
               NSLog(@"User has been updated (ASYNC): %@", updatedUser);
               [user updateProperties:@{@"name" : self.textFieldName.text}];
+//              [user updateProperties:@{@"floor" : self.floorTextField.text}];
           }
           error:^(Fault *fault) {
               NSLog(@"Server reported an error (ASYNC): %@", fault);
